6260617b3c7861d666cd1acff61b7480b5a4ff65
malexj <alexmaximovj@gmail.com>
2025-01-31 23:16:16 +0200

    Add loger and refactor

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import "./App.css";
 import "./index.css";
 import "./reports/instrument";
+import log from "loglevel";
 import UserPage from "./component/UserPage";
 import {useEffect, useState} from "react";
 
 function App() {
 
-  const [telegramWebApp, setTelegramWebApp] = useState(null);
   const [user, setUser] = useState(null);
-  const [tgInitDataUnsafe, setTgInitDataUnsafe] = useState(null);
-  const [tgInitData, setTgInitData] = useState(null);
   const [theme, setTheme] = useState("light");
-  const [params, setParams] = useState({});
 
   useEffect(() => {
     const tg = window.Telegram?.WebApp;
-    setTelegramWebApp(window.Telegram);
-
-    const queryParams = new URLSearchParams(window.location.search);
-    const paramsObject = {};
-
-    queryParams.forEach((value, key) => {
-      paramsObject[key] = value;
-    });
-
-    setParams(paramsObject);
 
     if (tg) {
       tg.ready();
@@ -35,74 +22,30 @@ function App() {
 
       // Initialize Telegram Web App API
       setUser(tg.initData?.user || tg.initDataUnsafe?.user || null);
-      setTgInitData(tg.initData || null);
-      setTgInitDataUnsafe(tg.initDataUnsafe || null);
       setTheme(tg.colorScheme);
 
-      // Show the main button
-      tg.MainButton.text = "Send Data";
-      tg.MainButton.show();
-
-      // Handle main button click
-      tg.MainButton.onClick(() => {
-        tg.sendData("This is data from the WebApp");
-        alert("main button clicked");
-      });
       // Cleanup
       return () => {
         tg.MainButton.offClick();
       };
     } else {
-      console.warn(
-          "Telegram Web App API is not available. Running outside Telegram.");
+      log.warn("Telegram Web App running outside Telegram.");
     }
   }, []);
 
-  // useEffect(() => {
-  //   const onResize = () => {
-  //     const isIos = window.Telegram.WebApp.platform === "ios";
-  //     const isIphone13Size = window.Telegram.WebApp.viewportStableHeight >= 670;
-  //
-  //     if (!isIos || (isIos && isIphone13Size)) {
-  //       document.body.style.setProperty("overflow", "hidden");
-  //       document.body.style.setProperty("overscroll-behavior", "none");
-  //     }
-  //   };
-  //   window.Telegram.WebApp.onEvent("viewportChanged", onResize);
-  //   return () => window.Telegram.WebApp.offEvent("viewportChanged", onResize);
-  // }, []);
-
-  return (<div
-      // Use theme to conditionally style the app
-      className={`App ${theme === "dark" ? "dark-theme" : "light-theme"}`}
-  >
-    <div>
-      <div>
-        telegramWebApp: {JSON.stringify(telegramWebApp, null, 1)}
-      </div>
-      <h1>Telegram Web App</h1>
-      {user ? (<p>
-        Hello, {user.first_name} {user.last_name || ""}!
-      </p>) : (<p>
-        Running locally. Open this app inside Telegram for full
-        functionality.
-      </p>)} <br/>
-      <br/>
-      <p>
-        tg init data: {JSON.stringify(tgInitData, null, 1)}
-      </p>
-      <br/>
-      <p>
-        Unsafe tg init data: {JSON.stringify(tgInitDataUnsafe, null, 1)}
-      </p>
-      <br/>
-      <p>
-        Start params: {JSON.stringify(params, null, 2)}
-      </p>
-      <br/>
-    </div>
-    <UserPage/>
-  </div>);
+  return (
+      <div className={`App ${theme === "dark" ? "dark-theme" : "light-theme"}`}>
+        <div>
+          <h1>Telegram Web App</h1>
+          {user ? (<p>
+            Hello, {user.first_name} {user.last_name || ""}!
+          </p>) : (<p>
+            Running locally. Open this app inside Telegram for full
+            functionality.
+          </p>)}
+        </div>
+        <UserPage/>
+      </div>);
 }
 
 export default App;
--- src/component/UserPage.js
+++ src/component/UserPage.js
@@ -1,155 +1,44 @@
 import React, {useCallback, useEffect, useState} from "react";
+import log from "loglevel";
 
 export default function UserPage() {
-  // List of users
-  const [users, setUsers] = useState([]);
 
-  // State for API errors
-  const [errorMessage, setErrorMessage] = useState(null);
-
-  // User ID from the form
-  const [userId, setUserId] = useState("");
-
-  // Username from the form
-  const [userFirstName, setUserFirstName] = useState("");
+  const [user, setUser] = useState([]);
 
   const backendUrl = process.env.REACT_APP_SERVER_URL;
-  const testTgInitData = window.Telegram?.WebApp.initData || process.env.REACT_APP_TEST_TG_INIT_DATA;
+  const testTgInitData = window.Telegram?.WebApp.initData ||
+      process.env.REACT_APP_TEST_TG_INIT_DATA;
 
-  // Memoize the fetchUsers function using useCallback
   const fetchUsers = useCallback(async () => {
-    try {
-      const response = await fetch(backendUrl, {
-        method: "GET", headers: {
-          "Content-Type": "application/json", "X-Auth-Token": testTgInitData,
-        },
-      });
-      if (!response.ok) {
-        console.error("Error fetching the list of users");
-        return;
-      }
-      const data = await response.json();
-      const {users} = data;
-      // Ensure 'users' exists in the response
-      if (users) {
-        // Set the users data from the response
-        setUsers(users);
-      } else {
-        console.error("No users data found");
-      }
-    } catch (error) {
-      console.error("Fetch error:", error);
-    }
-  }, [backendUrl]); // Dependency array to re-run if backendUrl changes
+    await fetch(backendUrl, {
+      method: "GET", headers: {
+        "Content-Type": "application/json", "X-Auth-Token": testTgInitData,
+      },
+    }).then(response => {
+      return response.json();
+    }).then(user => {
+      setUser(user);
+    }).catch(error => {
+      log.error("Error fetching the list of users", error);
+    });
+  }, [backendUrl, testTgInitData]);
 
   // Fetch the list of users from the backend
   useEffect(() => {
     const fetchData = async () => {
-      await fetchUsers(); // Await fetchUsers call
+      await fetchUsers();
     };
 
-    // Call the async function inside useEffect
     fetchData().catch((error) => {
-      console.error("Error in fetching data:", error);
+      log.error("Error in fetching user by tgInitData:", error);
     });
-  }, [fetchUsers]); // Now fetchUsers is stable and added as a dependency
-
-  // Handle adding a new user
-  const handleAddUser = async () => {
-    if (!userId || !userFirstName) {
-      alert("Please fill out all fields");
-      return;
-    }
-
-    const newUser = {userId: userId, first_name: userFirstName};
-
-    try {
-      const response = await fetch(backendUrl, {
-        method: "POST", headers: {
-          "Content-Type": "application/json", "X-Auth-Token": `${testTgInitData}`,
-        }, body: JSON.stringify(newUser),
-      });
-
-      if (!response.ok) {
-        console.error("Error adding the user");
-        // Reset form fields
-        setUserId("");
-        setUserFirstName("");
-        setErrorMessage("Error adding the user");
-        return;
-      }
-
-      // Update the user list
-      await fetchUsers();
-      // setUsers((prevUsers) => [...prevUsers, newUser]);
-
-      // Reset form fields
-      setUserId("");
-      setUserFirstName("");
-      setErrorMessage(null);
-    } catch (error) {
-      setErrorMessage("Error adding the user"); // Catch and set the error message
-      console.error("Error adding user:", error);
-    }
-  };
+  }, [fetchUsers]);
 
   return (<div
       className="min-h-screen bg-gray-100 flex flex-col items-center py-10">
     <div className="w-full max-w-2xl bg-white shadow-md rounded-2xl p-6">
       <h1 className="text-2xl font-bold mb-6 text-blue-700">Users Page</h1>
-
-      {/* User creation form */}
-      <div className="flex flex-col sm:flex-row gap-4 mb-6">
-        <input
-            type="text"
-            value={userId}
-            onChange={(e) => setUserId(e.target.value)}
-            placeholder="User ID"
-            className="flex-1 px-4 py-2 border rounded-xl shadow-sm focus:outline-blue-500"
-        />
-        <br/>
-        <br/>
-        <input
-            type="text"
-            value={userFirstName}
-            onChange={(e) => setUserFirstName(e.target.value)}
-            placeholder="User first name"
-            className="flex-1 px-4 py-2 border rounded-xl shadow-sm focus:outline-blue-500"
-        />
-        <br/>
-        <br/>
-        <button
-            onClick={handleAddUser}
-            className="bg-blue-600 text-white px-6 py-2 rounded-xl hover:bg-blue-700 transition">
-          Create
-        </button>
-      </div>
-      <br/>
-      {errorMessage && (<div
-          style={{color: "red"}}
-          className="mt-4 p-2 bg-red-100 text-red-600 border border-red-400 rounded">
-        {errorMessage}
-      </div>)}
-      <br/>
-      {/* List of users */}
-      <ul className="divide-y divide-gray-200">
-        {users.map(
-            (user) => (<li key={user.id} className="py-4 flex justify-between">
-              <div>
-                    <span
-                        className="text-gray-700">User id: {user.id}, userId : {user.userId}</span><br/>
-                <span className="text-gray-500">Info: {JSON.stringify(
-                    user.userInfo, null, 1)}</span><br/>
-                <span className="text-gray-500">Refs: {JSON.stringify(
-                    user.references, null, 1)}</span>
-                <br/>
-                <br/>
-              </div>
-            </li>))}
-      </ul>
-
-      {users.length === 0 &&
-          (<p className="text-gray-500 text-center mt-4">No users found</p>)}
+      {JSON.stringify(user, null, 2)}
     </div>
   </div>);
 }
